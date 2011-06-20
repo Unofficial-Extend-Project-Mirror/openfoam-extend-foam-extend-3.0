e099d0e5da098274fcf28d28188c3d12851cffb6
Ivor Clifford <ivor.clifford@psu.edu>
2011-06-20 19:52:05 -0400

    Required features for blockFvMatrix (in progress). ExpandContract support for diagTensor and sphericalTensor added. Enabled cloning of CoeffField<scalar>.

--- src/OpenFOAM/fields/expandContract/expandTensorField.C
+++ src/OpenFOAM/fields/expandContract/expandTensorField.C
@@ -32,7 +32,11 @@ Author
 \*---------------------------------------------------------------------------*/
 
 #include "tensorField.H"
+#include "diagTensorField.H"
+#include "sphericalTensorField.H"
 #include "expandTensor.H"
+#include "expandDiagTensor.H"
+#include "expandSphericalTensor.H"
 
 #define TEMPLATE
 #include "FieldFunctionsM.C"
@@ -51,7 +55,34 @@ void contractScalar(Field<scalar>& res, const UList<tensor>& f)
 }
 
 
+void contractScalar(Field<scalar>& res, const UList<diagTensor>& f)
+{
+    forAll (res, i)
+    {
+        contractScalar(res[i], f[i]);
+    }
+}
+
+
+void contractScalar(Field<scalar>& res, const UList<sphericalTensor>& f)
+{
+    forAll (res, i)
+    {
+        contractScalar(res[i], f[i]);
+    }
+}
+
+
 void contractLinear(Field<vector>& res, const UList<tensor>& f)
+{
+    forAll (res, i)
+    {
+        contractLinear(res[i], f[i]);
+    }
+}
+
+
+void contractLinear(Field<vector>& res, const UList<diagTensor>& f)
 {
     forAll (res, i)
     {
@@ -78,7 +109,34 @@ void expandScalar(Field<tensor>& res, const UList<scalar>& f)
 }
 
 
+void expandScalar(Field<diagTensor>& res, const UList<scalar>& f)
+{
+    forAll (res, i)
+    {
+        expandScalar(res[i], f[i]);
+    }
+}
+
+
+void expandScalar(Field<sphericalTensor>& res, const UList<scalar>& f)
+{
+    forAll (res, i)
+    {
+        expandScalar(res[i], f[i]);
+    }
+}
+
+
 void expandLinear(Field<tensor>& res, const UList<vector>& f)
+{
+    forAll (res, i)
+    {
+        expandLinear(res[i], f[i]);
+    }
+}
+
+
+void expandLinear(Field<diagTensor>& res, const UList<vector>& f)
 {
     forAll (res, i)
     {
